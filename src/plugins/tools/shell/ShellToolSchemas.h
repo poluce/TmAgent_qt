b1544fbfd317213d09d2a00394bf914cf795e84a
裴皖申 <peiwanshen@joule-instruments.com>
2026-04-21 14:15:24 +0800

    删除

--- src/plugins/tools/shell/ShellToolSchemas.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHELLTOOLSCHEMAS_H
-#define SHELLTOOLSCHEMAS_H
-
-#include <tmagent/types/ToolTypes.h>
-#include <QList>
-
-QList<TmAgent::Tool> shellTools();
-
-#endif // SHELLTOOLSCHEMAS_H
--- src/plugins/tools/workspace/WorkspaceToolSchemas.h
+++
@@ -1,9 +0,0 @@
-#ifndef WORKSPACETOOLSCHEMAS_H
-#define WORKSPACETOOLSCHEMAS_H
-
-#include <tmagent/types/ToolTypes.h>
-#include <QList>
-
-QList<TmAgent::Tool> workspaceTools();
-
-#endif // WORKSPACETOOLSCHEMAS_H
